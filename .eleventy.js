ee62c085903898f430dbf87274171aa03e67ccf4
webprofusion-chrisc <christopher.cook@webprofusion.com>
2022-02-21 17:26:34 +0800

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+
+  // custom filter for data lists : https://www.11ty.dev/docs/languages/liquid/#multiple-filter-arguments
+  eleventyConfig.addFilter("categoryFilter", function (items, category) {
+    return items.filter(f => f.categories?.includes(category));
+  });
+
+  if (process.env.ELEVENTY_PRODUCTION) {
+    eleventyConfig.addTransform("htmlmin", htmlminTransform);
+  } else {
+    eleventyConfig.setBrowserSyncConfig({ callbacks: { ready: browserSyncReady } });
+  }
+
+  // Passthrough
+  eleventyConfig.addPassthroughCopy({ "src/static": "." });
+
+  // Watch targets
+  eleventyConfig.addWatchTarget("./src/styles/");
+
+  var pathPrefix = "";
+  if (process.env.GITHUB_REPOSITORY) {
+    pathPrefix = process.env.GITHUB_REPOSITORY.split('/')[1];
+  }
+
+  return {
+    dir: {
+      input: "src"
+    },
+    pathPrefix
+  }
+};
+
+function browserSyncReady(err, bs) {
+  bs.addMiddleware("*", (req, res) => {
+    const content_404 = fs.readFileSync('_site/404.html');
+    // Provides the 404 content without redirect.
+    res.write(content_404);
+    // Add 404 http status code in request header.
+    // res.writeHead(404, { "Content-Type": "text/html" });
+    res.writeHead(404);
+    res.end();
+  });
+}
+
+function htmlminTransform(content, outputPath) {
+  if (outputPath.endsWith(".html")) {
+    let minified = htmlmin.minify(content, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true
+    });
+    return minified;
+  }
+  return content;
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# acme-clients
-The definitive list of ACME clients for Let's Encrypt and other ACME enabled CAs
+# ACME Clients 🌬️
+
+This is a resource to extensively document the available ACME Client options.
+
+https://acmeclients.com
+
+This sites use 11ty as a static site (Windty is a basic template using 11ty and Tailwind, and deploys to github pages).
+
+## How to Dev/Build
+
+1. Install dependencies: `npm install`
+2. Start development: `npm start`
+3. See your website at http://localhost:8080/
+4. To build the release version: `npm run build`
